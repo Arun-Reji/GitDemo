27b5069090f7653291c398f9788c1de511b4ff71
Arun <arunrejipallath@gmail.com>
2025-05-16 21:34:56 +0530

    Asian time commit

--- Introduction/src/actions.java
+++ Introduction/src/actions.java
@@ -13,6 +13,9 @@ public class actions {
 		driver.get("https://www.amazon.in/?&tag=googhydrabk1-21&ref=pd_sl_5szpgfto9i_e&adgrpid=155259813593&hvpone=&hvptwo=&hvadid=674893540034&hvpos=&hvnetw=g&hvrand=10986945815492740489&hvqmt=e&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=9062037&hvtargid=kwd-64107830&hydadcr=14452_2316413&gad_source=1");
 		Actions a = new Actions(driver);
 		a.moveToElement(driver.findElement(By.id("nav-link-accountList-nav-line-1"))).build().perform();
+		System.out.println("a");
+		System.out.println("a");
+		System.out.println("a");
 		
 		a.moveToElement(driver.findElement(By.id("twotabsearchtextbox"))).click().keyDown(Keys.SHIFT).sendKeys("hello Arun").doubleClick().build().perform();
 
